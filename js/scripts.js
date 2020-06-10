dfafdcbe2a1938ab6746841018d349f65d01a319
Taylor Phillips <taylorphillips133@gmail.com>
2020-06-10 15:01:17 -0700

    add beginning of js loop to check for vowels in index[0]
    Co-Authored-By: Nikolas Kososik <nikolas.kososik@gmail.com>
    Co-Authored-By: Sean Downs <spdowns@protonmail.com>

--- js/scripts.js
+++ js/scripts.js
@@ -3,21 +3,34 @@ function checkIfNumber(inputString)  {
   let finalArray = [];
   let stringArray = inputString.split("");
   const numArray = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
+  const vowelArray = ["a", "e", "i", "o", "u", "y", "A", "E", "I", "O", "U", "Y"];
+  
 
-    console.log(stringArray)
+    console.log(stringArray);
+    stringArray.forEach(checkIfVowel(inputString))  {
+  if (vowelArray.includes(inputString[0]))  {
+    finalArray.push(inputString + "yay");
+  } else {
+    finalArray.push(stringArray);
+  }
+// })
 
 
+//   for (let index=0; index<=stringArray.length-1; index+=1)  {
+//     finalArray.push(stringArray[index]);
+//     for (let j=0; j<=numArray.length-1; j+=1)  {
+//       if (numArray[j] === stringArray[index])  {
+//         finalArray.push();
+//       } else if (stringArray[index] === )
+//       };
+//     };
+//   };
+//   return finalArray.join("");
+// };
 
-  for (let index=0; index<=stringArray.length-1; index+=1)  {
-    finalArray.push(stringArray[index]);
-    for (let j=0; j<=numArray.length-1; j+=1)  {
-      if (numArray[j] === stringArray[index])  {
-         finalArray.push();
-      };
-    };
-  };
-  return finalArray.join("");
-};
+// function checkIfVowel(inputString)
+
+
 //  User Interface Logic
 $(document).ready(function() {
   $("form#pig").submit(function(event) {
