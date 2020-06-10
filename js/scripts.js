a33a6581c5dc03673eba74281ce34130b691ea54
Taylor Phillips <taylorphillips133@gmail.com>
2020-06-10 13:44:12 -0700

    add a working for loop to detect if numbers are being used
    Co-Authored-By: Nikolas Kososik <nikolas.kososik@gmail.com>
    Co-Authored-By: Sean Downs <spdowns@protonmail.com>

--- js/scripts.js
+++ js/scripts.js
@@ -4,20 +4,25 @@ function checkIfNumber(inputString)  {
   let stringArray = inputString.split("");
   const numArray = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
 
+    console.log(stringArray)
   for (let index=0; index<=stringArray.length-1; index+=1)  {
     finalArray.push(stringArray[index]);
-    for (let j=0; j<=numArray.length-1; j+1)  {
-      if  (numArray[j] === stringArray[index])  {
-        alert("You cannot input numbers!");
-      }
-      }
-}
-}
+    for (let j=0; j<=numArray.length-1; j+=1)  {
+      if (numArray[j] === stringArray[index])  {
+        finalArray.pop("")
+        alert("No Numbers")
+        break;
+      };
+    };
+  };
+  return finalArray.join("");
+};
 //  User Interface Logic
 $(document).ready(function() {
   $("form#pig").submit(function(event) {
   event.preventDefault();
   let inputString =  $("input#word").val();
-  $("#output").text(inputString);
+  let newString = checkIfNumber(inputString);
+  $("#output").text(newString);
   })
 })
