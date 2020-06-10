a9e73e5ea8d1ac194b2484d94827c57724b92940
Taylor Phillips <taylorphillips133@gmail.com>
2020-06-10 13:25:20 -0700

    add for loops to compare user inputs to known numbers so that an alert will be thrown when a number is given by user
    Co-Authored-By: Nikolas Kososik <nikolas.kososik@gmail.com>
    Co-Authored-By: Sean Downs <spdowns@protonmail.com>

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,23 @@
-$(document).ready(function() {
+//  Business Logic
+function checkIfNumber(inputString)  {
+  let finalArray = [];
+  let stringArray = inputString.split("");
+  const numArray = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
 
+  for (let index=0; index<=stringArray.length-1; index+=1)  {
+    finalArray.push(stringArray[index]);
+    for (let j=0; j<=numArray.length-1; j+1)  {
+      if  (numArray[j] === stringArray[index])  {
+        alert("You cannot input numbers!");
+      }
+      }
+}
+}
+//  User Interface Logic
+$(document).ready(function() {
+  $("form#pig").submit(function(event) {
+  event.preventDefault();
+  let inputString =  $("input#word").val();
+  $("#output").text(inputString);
+  })
 })
